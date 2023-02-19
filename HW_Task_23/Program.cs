10619e2bb7cd0a63dfa3e1e1cecffdddc9685ad4
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-19 18:08:11 +0300

    Home work - Task 23

---
+++ HW_Task_23/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("PLease input a number: ");
+int input_number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Please input value of exponent: ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine();
+System.Console.WriteLine("Result: ");
+for (int index = 1; index<=input_number; index++)
+{
+    int calculated_number = Convert.ToInt32(Math.Pow(index, exponent));
+    System.Console.Write(calculated_number);
+    System.Console.Write(" / ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine();
