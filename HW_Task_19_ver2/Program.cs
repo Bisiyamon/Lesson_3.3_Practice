36b593f24a225065cf85eed600b415d78d2bc539
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-19 14:42:42 +0300

    Home work - Task 19 - version 2

---
+++ HW_Task_19_ver2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача - проверить введенные данные являются ли словом или числом, и являются ли эти данные полиномом.
+//Второй вариант - перевели оба массива в строки и сравнили их
+
+//Вводим слово/число
+Console.Write("Input your number: ");
+string num = Console.ReadLine();
+
+//Переводим данные в массив из символов
+char[] number1 = num.ToCharArray();
+char[] number2 = num.ToCharArray();
+
+Array.Reverse(number2);
+
+bool isNumeric = int.TryParse(num, out _); //логическая переменная для проверки на число
+
+Console.Write("You entered: ");
+foreach (char ch in number1)
+{
+    Console.Write(" / ");
+    Console.Write(ch);
+}
+Console.WriteLine(" /");
+
+Console.Write("Reverse:     ");
+foreach (char ch in number2)
+{
+    Console.Write(" / ");
+    Console.Write(ch);
+}
+Console.WriteLine(" /");
+
+string array = new string(number1);
+string reverse_array = new string(number2);
+
+Console.WriteLine();
+if (array == reverse_array & isNumeric == true)
+{
+    Console.WriteLine("The entered number is polinom");
+}
+else if (array != reverse_array & isNumeric == true)
+{
+    Console.WriteLine("The entered number isn't polinom");
+}
+else if (array == reverse_array & isNumeric == false)
+{
+    Console.WriteLine("The entered word is polinom");
+}
+else if (array != reverse_array & isNumeric == false)
+{
+    Console.WriteLine("The entered word isn't polinom");
+}
+Console.WriteLine();
